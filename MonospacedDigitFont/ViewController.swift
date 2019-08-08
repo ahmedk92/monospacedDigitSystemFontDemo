d8652ef43ad08c88b5e4cbbc7930473905e2aa8d
Ahmed Khalaf <akhalaf@arabia-it.com>
2019-08-08 10:54:31 +0200

    Initial Commit

---
+++ MonospacedDigitFont/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonospacedDigitFont
+//
+//  Created by Arabia -IT on 8/8/19.
+//  Copyright © 2019 Arabia-IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
